83f9ef0f6b01662c10a49df0370235afa9a4ce9f
Ashhar Ahmed <42092122+AshharHibronan@users.noreply.github.com>
2019-06-19 10:07:56 +0530

    Changes on Course model
    Add new attributes for course model

--- BackEnd/NodeJS/Node/models/course.model.schema.js
+++ BackEnd/NodeJS/Node/models/course.model.schema.js
@@ -1,11 +1,15 @@
 const mongoose=require('mongoose');
 
 const CourseSchema = new mongoose.Schema({
+    
     CourseName : String,
     InchargLecture : String,
     CourseId : String,
     NumberOfEnrolledStudent : Number,
-    yearOfCourse : Number,
+    Year : Number,
+    Semester : String,
+    Faculty : String,
+    Department : String,    
     CourseAddedDate : String
 
 });
